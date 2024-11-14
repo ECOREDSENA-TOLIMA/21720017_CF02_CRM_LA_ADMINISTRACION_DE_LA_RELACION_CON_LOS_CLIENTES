b417c11ef65429ac966258cea2409c518493be86
Francisco Vasquez <fjvasquez8@misena.edu.co>
2024-11-13 19:18:21 -0500

    Avance
    Avance

--- src/config/global.js
+++ src/config/global.js
@@ -216,7 +216,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CFA_02_21720017_DU.pdf',
       },
       {
         icono: 'fas fa-download',
